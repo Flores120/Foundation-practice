b0d946925aba6dcb2e8f7e9f9f02ba227966adb2
Alvaro Flores <alvaro.flores01@gmail.com>
2016-12-01 16:44:51 -0800

    more advanced layout added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
 $(document).foundation();
+function backgroundB() {
+    document.body.style.backgroundImage = "url('http://cdn.wallpapersafari.com/66/93/VvORQZ.jpg')";
+    document.getElementById('top-bar').style.backgroundColor = "#1C2836";
+    document.getElementById('bottom').style.backgroundColor = "#1C2836";
+    document.getElementById('search').style.backgroundColor = "#1C2836";
+
+}
+function backgroundW() {
+    document.body.style.backgroundImage = "url('http://fullhdpictures.com/wp-content/uploads/2016/08/HD-Blue-Sky-Wallpaper.jpg')";
+    document.getElementById('top-bar').style.backgroundColor = "#E6E6E6";
+    document.getElementById('bottom').style.backgroundColor = "#E6E6E6";
+    document.getElementById('search').style.backgroundColor = "#E6E6E6";
+
+}
